a6acdd0c3848caec8f902d51d759d1955230bd42
Firdavs Bagirov <firdavsbagirov@abdigital.uz>
2022-01-30 15:15:50 +0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Firdavs Bagirov on 30/01/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
